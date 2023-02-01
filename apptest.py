a7cd268605ef3245253384f3930e55116db8dbee
“Ryan <ryan_sise@fastmail.com>
2023-02-01 11:14:18 -0700

    Added test for valid return value

--- apptest.py
+++ apptest.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 
 from lib.backend import get_img
 
@@ -10,6 +11,16 @@ class TestAPIResponse(unittest.TestCase):
         api_key = " "
         data_entry = get_img(api_key, date, rover)
         self.assertEqual(data_entry['error']['code'], "API_KEY_INVALID")
+    
+    def test_backend_return(self):
+        api_key = os.path.join( os.getcwd(), 'api_key.txt' )
+        date = "2022-12-01"
+        rover = "curiosity"
+        data_entry = get_img(api_key, date, rover)
+        leading = data_entry[0][0:4]
+        trailing = data_entry[0][-3:]
+        self.assertEqual(leading,"https")
+        self.assertEqual(trailing,"JPG")
 
 if __name__ == '__main__':
         unittest.main()
