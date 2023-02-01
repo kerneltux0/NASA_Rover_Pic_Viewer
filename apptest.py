adddc9f0b3811786b3d3c7252106a7a9b96bf379
“Ryan <ryan_sise@fastmail.com>
2023-02-01 10:34:44 -0700

    Created test for API response

--- apptest.py
+++ apptest.py
@@ -1,2 +1,10 @@
 import unittest
 
+from lib.backend import get_img
+
+def test_api_response(self):
+    api_key = " "
+    date = "2022-12-01"
+    rover = "curiosity"
+    data_entry = get_img(api_key, date, rover)
+    self.assertEqual(data_entry['error']['code'], "API_KEY_INVALID")
