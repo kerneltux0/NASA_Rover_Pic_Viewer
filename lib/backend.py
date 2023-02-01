d0c09bf4f8740221c437ad0fa56d4019df968288
“Ryan <ryan_sise@fastmail.com>
2023-02-01 12:02:28 -0700

    Added code to pass return value test

--- lib/backend.py
+++ lib/backend.py
@@ -1,7 +1,14 @@
 import requests
 
+
 def get_img(api_key,date,rover):
     base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
     req_url = base_url + rover + "/photos?earth_date=" + date + "&api_key=" + api_key
     response = requests.get(req_url).json()
-    return response
+    photo_json = response['photos']
+
+    photos = []
+    for i in photo_json:
+        photos.append(i['img_src'])
+    return photos
+
