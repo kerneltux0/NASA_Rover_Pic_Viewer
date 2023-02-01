02dcaf4d366b8575e75812c1fc3e2fc48d8b8944
“Ryan <ryan_sise@fastmail.com>
2023-02-01 11:01:57 -0700

    Fixed structural issue causing false positives

--- apptest.py
+++ apptest.py
@@ -2,9 +2,14 @@ import unittest
 
 from lib.backend import get_img
 
-def test_api_response(self):
-    api_key = " "
-    date = "2022-12-01"
-    rover = "curiosity"
-    data_entry = get_img(api_key, date, rover)
-    self.assertEqual(data_entry['error']['code'], "API_KEY_INVALID")
+class TestAPIResponse(unittest.TestCase):
+    
+    def test_api_response(self):
+        date = "2022-12-01"
+        rover = "curiosity"
+        api_key = " "
+        data_entry = get_img(api_key, date, rover)
+        self.assertEqual(data_entry['error']['code'], "API_KEY_INVALID")
+
+if __name__ == '__main__':
+        unittest.main()
