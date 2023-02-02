05f6d663ed647f26086a84e399faff1e1f0b429b
“Ryan <ryan_sise@fastmail.com>
2023-02-02 07:27:44 -0700

    Fixed conditional in resp to API data

--- lib/backend.py
+++ lib/backend.py
@@ -1,13 +1,11 @@
 import requests
-import code
 
 
 def get_img(api_key,date,rover):
     base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
     req_url = base_url + rover + "/photos?earth_date=" + date + "&api_key=" + api_key
     response = requests.get(req_url).json()
-    code.interact(local=dict(globals(), **locals()))
-    if response['error']:
+    if 'error' in response:
         return response
     else:
         photo_json = response['photos']
