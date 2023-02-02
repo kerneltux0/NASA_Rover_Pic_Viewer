531eea294811c33d4f70b59e53179ea0bde78292
“Ryan <ryan_sise@fastmail.com>
2023-02-02 08:25:12 -0700

    Wrote test for searching API

--- apptest.py
+++ apptest.py
@@ -2,6 +2,7 @@ import unittest
 import os
 
 from lib.backend import get_img
+from factory import Factory, search_api_imgs
 
 class TestAPIResponse(unittest.TestCase):
     
@@ -24,6 +25,20 @@ class TestAPIResponse(unittest.TestCase):
         trailing = data_entry[0][-3:]
         self.assertIn("http",leading)
         self.assertEqual(trailing,"JPG")
+
+    def test_api_search(self):
+        api_key_file = os.path.join( os.getcwd(), 'api_key.txt' )
+        file = open(api_key_file)
+        api_key = file.readline()
+        file.close()
+        date = "2022-11-01"
+        rover = "curiosity"
+        data = search_api_imgs(api_key,date,rover)
+        self.assertIsInstance(data,Factory)
+        self.assertEqual(data.date,date)
+        self.assertEqual(data.rover,rover)
+        self.assertNotEqual(data.images.count,0)
+        
 
 if __name__ == '__main__':
         unittest.main()
