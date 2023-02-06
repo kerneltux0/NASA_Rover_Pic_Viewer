80221dadd01a9a5ef504c2fe368255f352b514f8
“Ryan <ryan_sise@fastmail.com>
2023-02-06 09:14:40 -0700

    separated conditional to its own function

--- lib/api_query.py
+++ lib/api_query.py
@@ -5,12 +5,14 @@ def get_img(api_key,date,rover):
     base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
     req_url = base_url + rover + "/photos?earth_date=" + date + "&api_key=" + api_key
     response = requests.get(req_url).json()
-    if 'error' in response:
-        return response
+    return eval_payload(response)
+
+def eval_payload(data):
+    if 'error' in data:
+        return data
     else:
-        photo_json = response['photos']
+        photo_json = data['photos']
         photos = []
         for i in photo_json:
             photos.append(i['img_src'])
         return photos
-
