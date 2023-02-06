bf39bee2e2117a23c35cfaf70f612512bda599da
“Ryan <ryan_sise@fastmail.com>
2023-02-06 09:25:51 -0700

    re-factored to pass test

--- lib/ui_backend.py
+++ lib/ui_backend.py
@@ -1,7 +1,8 @@
 from datetime import date
+import random
 
 def year_generator(yr_low,yr_high):
-    year = range(yr_low,yr_high)
+    year = random.randint(yr_low,yr_high)
     return year
 
 #def month_day():
