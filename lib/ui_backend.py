d57ba5d5193c55f09b98a599f604ec5edfb9dfe3
“Ryan <ryan_sise@fastmail.com>
2023-02-06 12:36:59 -0700

    added req'd imports added run_search re-factored formatting output of month_day

--- lib/ui_backend.py
+++ lib/ui_backend.py
@@ -1,4 +1,6 @@
 from datetime import date
+from key_file import get_api_key
+from lib.api_query import get_img
 import random
 
 def year_generator(yr_low,yr_high):
@@ -9,5 +11,10 @@ def month_day():
     today = date.today()
     month = today.strftime('%m')
     day = today.strftime('%d')
-    on_this_day = month + day
+    on_this_day = month + "-" + day
     return on_this_day
+
+def run_search(date,rover):
+    key = get_api_key()
+    images = get_img(key,date,rover)
+    
